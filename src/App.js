c2ce0b0b4969144d9589718c6102921ee50811b8
David E. Barrera <dbarrerap@live.com>
2022-02-16 20:21:32 -0500

    Add AppBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import NavigationBar from './components/NavigationBar'
+
 function App() {
   return (
     <div className="App">
-      
+      <NavigationBar />
     </div>
   );
 }
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant='h4' sx={{ flexGrow: 1, padding: 1 }}>Clima App</Typography>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+export default Navbar
